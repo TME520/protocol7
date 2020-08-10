90a3b7ad8eb41332bab0359deeb01e8f38f33ef1
Five Twenty <tme520@gmail.com>
2020-08-10 10:25:15 +1000

    CHanged DDB startup script: DDB now running in background

--- startDynamoDB.sh
+++ startDynamoDB.sh
@@ -1,9 +1,8 @@
-#! /bin/bash
+#!/bin/bash
 
 echo "Starting DynamoDB, listening on port 8001..."
-cd $HOME/dynamodb_local_latest/
-pwd
-java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -port 8001
+cd $HOME/dynamodb/
+nohup java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -port 8001 &
 echo "...done."
 
 exit 0
