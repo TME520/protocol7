6e41c57380e85319ccfa57ee16f448a9bc5188e2
Five Twenty <tme520@gmail.com>
2020-11-08 16:33:50 +1100

    Implementing checkHTTPStatus - WIP - push 5

--- get_http_return_code.py
+++ get_http_return_code.py
@@ -1,11 +1,18 @@
+from urllib.error import HTTPError
 import urllib.request
 import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def getResponseCode(url):
-    conn = urllib.request.urlopen(url)
-    print(f'HTTP status for {url}: {conn.getcode()}')
-    return conn.getcode()
+    try:
+        conn = urllib.request.urlopen(url)
+        print(f'HTTP status for {url}: {conn.getcode()}')
+        return conn.getcode()
+    except HTTPError as e:
+        print(f'Error: {e.code}')
+    except Exception as e:
+        print(f'Other exception: {e}')
+        pass
 
 getResponseCode("https://netwealth.com.au")
