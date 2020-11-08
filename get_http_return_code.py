a0178c231356beaa212df3cf5352a227bb819def
Five Twenty <tme520@gmail.com>
2020-11-08 16:03:43 +1100

    Implementing checkHTTPStatus - WIP - push 3

--- get_http_return_code.py
+++ get_http_return_code.py
@@ -1,13 +1,8 @@
-import urllib2
-for url in ["http://entrian.com/", "http://entrian.com/does-not-exist/"]:
-    try:
-        connection = urllib2.urlopen(url)
-        print connection.getcode()
-        connection.close()
-    except urllib2.HTTPError, e:
-        print e.getcode()
+import urllib.request
 
-# Prints:
-# 200 [from the try block]
-# 404 [from the except block]
+def getResponseCode(url):
+    conn = urllib.request.urlopen(url)
+    print(f'HTTP status for {url}: {conn.getcode()}')
+    return conn.getcode()
 
+getResponseCode("https://netwealth.com.au")
