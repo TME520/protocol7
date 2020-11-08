f17920d13ecbf4af34bc11d252d934aad8377b1d
Five Twenty <tme520@gmail.com>
2020-11-08 15:59:14 +1100

    Implementing checkHTTPStatus - WIP - push 2

--- get_http_return_code.py
+++ get_http_return_code.py
@@ -1,7 +1,13 @@
-import requests
-try:
-    r = requests.head("https://netwealth.com.au")
-    print(r.status_code)
-    # prints the int of the status code. Find more at httpstatusrappers.com :)
-except requests.ConnectionError:
-    print("failed to connect")
+import urllib2
+for url in ["http://entrian.com/", "http://entrian.com/does-not-exist/"]:
+    try:
+        connection = urllib2.urlopen(url)
+        print connection.getcode()
+        connection.close()
+    except urllib2.HTTPError, e:
+        print e.getcode()
+
+# Prints:
+# 200 [from the try block]
+# 404 [from the except block]
+
